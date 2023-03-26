7c9c649408a765e22b8e030e28488b4470b0155a
Uriel <uriel1010@gmail.com>
2023-03-26 21:01:13 +0300

    finished lecture 16 - CNN

--- lstm.py
+++ lstm.py
@@ -7,6 +7,10 @@ DATASET_PATH = "data.json"
 
 
 def load_data(dataset_path):
+    """
+    :param dataset_path: a string representing the file path to the dataset
+    :return: a tuple containing the inputs and targets as numpy arrays
+    """
     with open(dataset_path, "r") as fp:
         data = json.load(fp)
 
@@ -17,6 +21,11 @@ def load_data(dataset_path):
 
 
 def prepare_datasets(test_size, validation_size):
+    """
+    :param test_size: a float representing the proportion of the dataset to be used as test set
+    :param validation_size: a float representing the proportion of the dataset to be used as validation set
+    :return: a tuple containing the train, validation, and test sets as numpy arrays, and their corresponding targets
+    """
 
     # load data
     X, y = load_data(DATASET_PATH)
@@ -28,39 +37,29 @@ def prepare_datasets(test_size, validation_size):
     X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=validation_size)
 
     # 3d array -> (130,13,1)
-    X_train = X_train[..., np.newaxis] #4d array -> (num_samples, 130, 13, 1)
-    X_validation = X_validation[..., np.newaxis]
-    X_test = X_test[..., np.newaxis]
+    # X_train = X_train[..., np.newaxis] #4d array -> (num_samples, 130, 13, 1)
+    # X_validation = X_validation[..., np.newaxis]
+    # X_test = X_test[..., np.newaxis]
 
     return X_train, X_validation, X_test, y_train, y_validation, y_test
 
 
 def build_model(input_shape):
+    """
+    :param input_shape: a tuple representing the shape of the input data
+    :return: a sequential model
+    """
 
-    # create model
+    # create RNN-LSTM model
     model = tf.keras.Sequential()
 
-    # 1st conv layer
-    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
-    model.add(tf.keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same'))
-    model.add(tf.keras.layers.BatchNormalization())
+    # 2 LSTM layers
+    model.add(tf.keras.layers.LSTM(64, input_shape=input_shape, return_sequences=True))
+    model.add(tf.keras.layers.LSTM(64))
 
-    # 2nd conv layer
-    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
-    model.add(tf.keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same'))
-    model.add(tf.keras.layers.BatchNormalization())
-
-    # 3rd conv layer
-    model.add(tf.keras.layers.Conv2D(32, (2, 2), activation='relu', input_shape=input_shape))
-    model.add(tf.keras.layers.MaxPool2D((2, 2), strides=(2, 2), padding='same'))
-    model.add(tf.keras.layers.BatchNormalization())
-
-
-    # flatten the output and feed it into dense layer
-    model.add(tf.keras.layers.Flatten())
+    # Dense layer
     model.add(tf.keras.layers.Dense(64, activation='relu'))
     model.add(tf.keras.layers.Dropout(0.3))
-
 
     # output layer
     model.add(tf.keras.layers.Dense(10, activation='softmax'))
@@ -68,6 +67,12 @@ def build_model(input_shape):
     return model
 
 def predict(model, X, y):
+    """
+    :param model: a Keras model used for making the prediction
+    :param X: a numpy array representing the input data
+    :param y: an integer representing the expected index
+    :return: None
+    """
     X = X[np.newaxis, ...]
 
     # prediction = [ [0.1, 0.2, ...] ]
@@ -83,7 +88,7 @@ if __name__ == "__main__":
     X_train, X_validation, X_test, y_train, y_validation, y_test = prepare_datasets(0.25, 0.2)
 
     # build the cnn net
-    input_shape = (X_train.shape[1], X_train.shape[2], X_train.shape[3])
+    input_shape = (X_train.shape[1], X_train.shape[2]) # 130, 13
     model = build_model(input_shape)
 
     # compile the network
@@ -92,8 +97,10 @@ if __name__ == "__main__":
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
+    model.summary()
+
     # train the cnn
-    model.fit(X_train, y_train, validation_data=(X_validation, y_validation), batch_size=32, epochs=40)
+    model.fit(X_train, y_train, validation_data=(X_validation, y_validation), batch_size=32, epochs=30)
 
     # evaluate the CNN on the test set
     test_error, test_accuracy = model.evaluate(X_test, y_test, verbose=1)
