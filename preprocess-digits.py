26b3387886ff16a431b16598431d6f9089279201
FiodarM <fyodormorozko95@gmail.com>
2023-05-03 23:05:13 +0300

    Fix preprocess-digits.py

--- preprocess-digits.py
+++ preprocess-digits.py
@@ -1,62 +1,61 @@
 import os
+import sys
+import glob
 import librosa
-import math
+from scipy.io import wavfile
 import warnings
 import numpy as np
+from tqdm import tqdm
 
-warnings.filterwarnings("ignore")
 
 DATASET_PATH = "free-spoken-digit-dataset/recordings"
-NPZ_PATH = "free-spoken-digit-dataset/digitsData.npz"
-
-SAMPLE_RATE = 22050
-DURATION = 1  # in seconds
-SAMPLES_PER_TRACK = SAMPLE_RATE * DURATION
+NPZ_PATH = "free-spoken-digit-dataset/digits_mfcc.npz"
 
 
-def save_mfcc_to_npz(dataset_path, npz_path, num_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
+def save_mfcc_to_npz(dataset_path, npz_path, num_mfcc=20, hop_length=128, n_fft=512):
     """
     Extracts MFCCs from the free spoken digit dataset and saves them into an NPZ file along with the digit labels.
 
     :param dataset_path: str, Path to the free spoken digit dataset directory
     :param npz_path: str, Path to the NPZ file used to save MFCCs, labels, and mapping
     :param num_mfcc: int, Number of coefficients to extract, default is 13
-    :param n_fft: int, Interval to consider when applying FFT, measured in number of samples, default is 2048
     :param hop_length: int, Sliding window for FFT, measured in number of samples, default is 512
-    :param num_segments: int, Number of segments to divide sample tracks into, default is 5
     :return: None
     """
-    # data lists to store mapping, labels, and MFCCs
-    mapping = []
-    labels = []
+    # data lists to store MFCCs with labels
     mfcc_data = []
+    labels = []
 
-    samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
-    num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
+    signals = []
+    max_length = 0
+    max_sample_rate = 0
+    files = glob.glob(os.path.join(dataset_path, '*.wav'))
+    for f in files:
+        sample_rate, signal = wavfile.read(f)
+        labels.append(os.path.basename(f))
+        signals.append(signal)
+        if signal.shape[0] > max_length:
+            max_length = signal.shape[0]
+        if sample_rate > max_sample_rate:
+            max_sample_rate = sample_rate
 
-    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
-        for f in filenames:
-            if f.endswith(".wav"):
-                if int(f[0]) not in mapping:
-                    mapping.append(int(f[0]))
-                file_path = os.path.join(dirpath, f)
-                signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
+    with tqdm(total=len(files)) as pbar:
+        for signal, label in zip(signals, labels):
+            pbar.set_description(f'Processing recording {label}')
+            signal = np.pad(signal, (0, max_length - signal.shape[0]))
+            mfcc = librosa.feature.mfcc(
+                y=signal.astype(np.double),
+                sr=max_sample_rate,
+                n_mfcc=num_mfcc,
+                hop_length=hop_length,
+                n_fft=n_fft
+            )
+            mfcc_data.append(mfcc.T)
+            pbar.update(1)
 
-                for d in range(num_segments):
-                    start = samples_per_segment * d
-                    finish = start + samples_per_segment
+    # # save MFCCs and labels to npz file
+    np.savez(npz_path, mfcc=mfcc_data, labels=labels)
 
-                    mfcc = librosa.feature.mfcc(signal[start:finish], sample_rate, n_mfcc=num_mfcc, n_fft=n_fft,
-                                                hop_length=hop_length)
-                    mfcc = mfcc.T
-
-                    if len(mfcc) == num_mfcc_vectors_per_segment:
-                        mfcc_data.append(mfcc.tolist())
-                        labels.append(int(f[0]))
-                        print("{}, segment:{}".format(file_path, d + 1))
-
-    # save MFCCs and labels to npz file
-    np.savez(npz_path, mfcc=mfcc_data, labels=labels, mapping=mapping)
 
 def load_data(npz_path):
     with np.load(npz_path) as data:
@@ -64,7 +63,7 @@ def load_data(npz_path):
         labels = data['labels']
     return mfcc, labels
 
+
 if __name__ == "__main__":
-    save_mfcc_to_npz(DATASET_PATH, NPZ_PATH, num_segments=10)
+    save_mfcc_to_npz(DATASET_PATH, NPZ_PATH)
     mfcc_data, labels = load_data(NPZ_PATH)
-    pass
